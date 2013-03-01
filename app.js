6d8184a5ee7ed70f57397134afffa7ca374eb28f
Ladislav Prskavec <ladislav@prskavec.net>
2013-03-01 17:56:22 +0100

    Update headers

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ app.enable('trust proxy');
 app.get('/item/1', function(req, res){
   var body = '{"id": 1, "name": "Item 1", "description": "Lorem ipsum ...", "done": false}';
   res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Content-Length', body.length);
   res.end(body);
 });
 
 app.get('/items', function(req, res){
   var body = '{"items":[{ "id":1 ,"name": "Item 1", "description": "Lorem ipsum ...", "done": false, "priority": 2 },{ "id":2 ,"name": "Item 2", "description": "Lorem Ipsum ...", "done": false, "priority": 1}]}';
   res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Content-Length', body.length);
   res.end(body);
 });
 
